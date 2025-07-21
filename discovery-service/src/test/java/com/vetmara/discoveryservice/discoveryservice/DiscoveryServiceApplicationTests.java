3d8e927d9a99c1ee4775a2005aaf0f2a1cb07d1f
Neider Lopez <lpzneider@gmail.com>
2025-07-20 23:20:42 -0500

    creacion y configuracion de discovery-service

---
+++ discovery-service/src/test/java/com/vetmara/discoveryservice/discoveryservice/DiscoveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vetmara.discoveryservice.discoveryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
